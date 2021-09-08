8ecf0d7a1a2c6644d17382c9b8737d46d9c98a11
shubhanshuyadav <shubhanshuemailid@gmail.com>
2021-09-08 17:49:09 +0530

    updateImage function not working properly

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
+let idInfo={"Name":"inputName","Father":"inputFatherName","Email":"inputEmail","Roll-No":"inputRollNo","Contact-No":"inputContactNo","Branch":"inputBranch","DOB":"inputDOB","Address":"inputAddress"}
+
+
 let setIdInfo=function(){
-    Validite()
-    let name =document.getElementById("inputFullName").value;
-    let position =document.getElementById("inputPosition").value;
-    let email =document.getElementById("inputEmail").value;
-    document.getElementById("name").innerHTML=name;
-    document.getElementById("position").innerHTML=position;
-    document.getElementById("email").innerHTML=email;
-    document.getElementById("card").setAttribute("style", "display:flex");
-    
+  uploadImage();
+    for(let info in idInfo){
+      document.getElementById(info).innerHTML=info+": "+document.getElementById(idInfo[info]).value;
+    }  
+    document.getElementById("card").style.display="grid";
+  }
+
+let uploadImage=function(){
+  console.log('1');
+
+  window.onunload = function () {
+    localStorage.removeItem("recent-image");
+  }
+  document.getElementById("inputImage").addEventListener("change", function(){
+    const reader = new FileReader();
+    console.log('2');
+    reader.addEventListener("load",() => {
+      console.log('3');
+      localStorage.setItem("recent-image",reader.result);
+      console.log(reader.result);
+    });
+    reader.readAsDataURL(this.files[0]);
+
+  });
+    const recentImageDataUrl=localStorage.getItem("recent-image");
+    if(recentImageDataUrl){
+      document.querySelector("#imageId").setAttribute("src",recentImageDataUrl);
+    }
 }
+
+
+
+
+  
+
 
 
