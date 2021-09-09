798012bff0e332eba8aa23f959f1b67388fde41c
shubhanshuyadav <shubhanshuemailid@gmail.com>
2021-09-09 14:33:57 +0530

    accesing image done

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 let cardInfo={"Name":"inputName","Father":"inputFatherName","Email":"inputEmail","Roll-No":"inputRollNo","Contact-No":"inputContactNo","Branch":"inputBranch","DOB":"inputDOB"}
+var loadFile = function(event) {
+	var image = document.getElementById('imageId');
+	image.src = URL.createObjectURL(event.target.files[0]);
+  console.log(image.src);
+}
 
 const addCardInfo=(e)=>{
 e.preventDefault();
-const Image=document.getElementById("inputImage").value;
-console.log(Image);
+console.log(document.getElementById("imageId").src);
 const Address=document.getElementById("Address");
 const College=document.getElementById("College-Name");
 const BackgroundColor=document.getElementById("inputBackgroundColor").value;
 const TextColor=document.getElementById("inputTextColor").value;
 Address.innerHTML=document.getElementById("inputAddress").value;
 College.innerHTML=document.getElementById("inputCollegeName").value;
-document.getElementById("imageId").src=Image;
 
 
 for(let info in cardInfo){
